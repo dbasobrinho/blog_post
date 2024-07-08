2c5ff6b9fa8e623e9f509fef580722d69a795192
dbasobrinho <166563862+dbasobrinho@users.noreply.github.com>
2024-07-08 11:54:03 -0300

    Add files via upload

---
+++ dbasobrinho.com.br_PLSQL_TRACE_02-tabela_teste.sql
@@ -0,0 +1,19 @@
+sqlplus sys/manager@pdb1 as sysdba
+
+
+begin execute immediate 'drop table SH.TX'; exception when others then null; end;
+/
+create table SH.TX  (EMPNO PRIMARY KEY, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO)
+as
+SELECT level                                                            as EMPNO
+      ,trim(DBMS_RANDOM.string ('X', TRUNC (DBMS_RANDOM.value (3,10)))) as ENAME
+      ,trim(DBMS_RANDOM.string ('X', TRUNC (DBMS_RANDOM.value (5,9))))  as JOB 
+	  ,DBMS_RANDOM.value(1,ROWNUM)                                      as MGR 
+	  ,SYSDATE - DBMS_RANDOM.value(0,366)                               as HIREDATE
+	  ,round(DBMS_RANDOM.value(1200,50000),2)                           as SAL
+	  ,trunc(DBMS_RANDOM.value(1,100))                                  as COMM
+	  ,trunc(DBMS_RANDOM.value(1,50))                                   as DEPTNO
+FROM   dual
+CONNECT BY level <= 300000;
+
+exec DBMS_STATS.GATHER_TABLE_STATS(ownname => 'SH',tabname  => 'TX');
